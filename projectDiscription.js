d1c04d6e49aba7cb7901cb72491d3df8883939bd
sam <95609691+sam337amen@users.noreply.github.com>
2024-09-28 23:57:46 +0300

    descriptin  for booking-room website

---
+++ projectDiscription.js
@@ -0,0 +1,14 @@
+/*
+----------  ABOUT BOOKING ROOM REACT APP DEVELOPMENT --------- 
+The Booking Room React App is a user-friendly web application designed to facilitate seamless room reservations.
+ Users can easily search for available rooms based on their preferences, such room type and room price. The intuitive
+  interface allows users to filter results and view detailed information about each room, includingimages, pricing,
+   and availability. Once they find a suitable option, they can proceed to book the room.
+
+On the administrative side, the app empowers admins with essential tools to manage the room database effectively.
+ Admins can add new rooms, edit existing room details, and delete rooms that are no longer available. The admin
+  dashboard is designed for ease of use, allowing for quick navigation and efficient management of the room listings,
+   ultimately enhancing the overall user experience.
+
+             --------- Prepared by @sam337amen-----------------
+*/
